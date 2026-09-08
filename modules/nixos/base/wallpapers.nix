15bb4622577bfee689b6909cbe9c7c9c96fb1ecf
Xisray <safixxkir@yandex.ru>
2026-09-08 19:31:33 +0500

    wallpapers is done

--- modules/nixos/base/wallpapers.nix
+++ modules/nixos/base/wallpapers.nix
@@ -7,11 +7,29 @@
       ...
     }:
     let
-      colors = builtins.attrValues config.lib.stylix.colors.withHashtag;
+      colors = with config.lib.stylix.colors.withHashtag; [
+        base00
+        base01
+        base02
+        base03
+        base04
+        base05
+        base06
+        base07
+        base08
+        base09
+        base0A
+        base0B
+        base0C
+        base0D
+        base0E
+        base0F
+      ];
       hald-clut =
         pkgs.runCommand "stylix-hald-clut.png"
           {
             nativeBuildInputs = [ pkgs.lutgen ];
+            inherit (config.stylix) base16Scheme;
           }
           ''
             lutgen generate -o $out -- ${lib.escapeShellArgs colors}
@@ -21,6 +39,7 @@
         version = inputs.wallpapers.rev or "unknown";
         src = inputs.wallpapers;
         nativeBuildInputs = [ pkgs.lutgen ];
+        inherit (config.stylix) base16Scheme;
         installPhase = ''
           mkdir -p $out
           for f in "$src"/*.{png,jpg,jpeg,webp}; do
