7e39aca09c9ec16f51fe23a4a9831b2007888f13
Xisray <safixxkir@yandex.ru>
2026-09-14 13:20:23 +0500

    added qbittorrent

--- modules/nixos/features/qbittorrent.nix
+++ modules/nixos/features/qbittorrent.nix
@@ -1,13 +1,19 @@
 {
-  flake.nixosModules.qbittorrent = { ... }: {
+  flake.nixosModules.qbittorrent = {
     services.qbittorrent = {
       enable = true;
       openFirewall = true;
+      serverConfig = {
+        Preferences.WebUI.LocalHostAuth = true;
+      };
     };
     users.users.qbittorrent = {
       isSystemUser = true;
       group = "qbittorrent";
     };
     users.groups.qbittorrent = { };
+    preferences.persistance.directories = [
+      "/var/lib/qBittorrent/qBittorrent"
+    ];
   };
 }
--- modules/nixos/hosts/nixos/configuration.nix
+++ modules/nixos/hosts/nixos/configuration.nix
@@ -18,11 +18,39 @@
       self.nixosModules.niri
       self.nixosModules.clashVerge
       self.nixosModules.syncthing
+      self.nixosModules.qbittorrent
       self.diskoConfigurations.nixos
     ];
 
-    preferences.hostName = hostName;
-    preferences.shell = "fish";
+    preferences = {
+      hostName = hostName;
+      shell = "fish";
+      mouse = {
+        accel-profile = "flat";
+        accel-speed = 0.0;
+      };
+      monitors = [
+        {
+          name = "Xiaomi Corporation Mi Monitor 5598910068895";
+          mode = "2560x1440@180.000";
+          position = {
+            x = 0;
+            y = 0;
+          };
+        }
+        {
+          name = "Acer Technologies Acer HS244HQ LS80W0094390";
+          # mode = "1920x1080@60.000";
+          # для разгона:
+          mode = "1920x1080@110";
+          custom = true;
+          position = {
+            x = 2560;
+            y = 0;
+          };
+        }
+      ];
+    };
 
     system.stateVersion = "26.05";
   };
