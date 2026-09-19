0a3150ebbd7e5b62176c4d33ed880deadfe141d5
Xisray <safixxkir@yandex.ru>
2026-09-19 12:38:05 +0500

    feat(kitty): added keybinds for tabs

--- modules/home/features/terminal/kitty.nix
+++ modules/home/features/terminal/kitty.nix
@@ -17,6 +17,17 @@
           shell_integration = "enabled";
           background_blur = 32;
         };
+        keybindings = {
+          "ctrl+1" = "goto_tab 1";
+          "ctrl+2" = "goto_tab 2";
+          "ctrl+3" = "goto_tab 3";
+          "ctrl+4" = "goto_tab 4";
+          "ctrl+5" = "goto_tab 5";
+          "ctrl+6" = "goto_tab 6";
+          "ctrl+7" = "goto_tab 7";
+          "ctrl+8" = "goto_tab 8";
+          "ctrl+9" = "goto_tab 9";
+        };
       };
       preferences.binds."Mod+Return".action = lib.getExe pkgs.kitty;
       home.sessionVariables = {
